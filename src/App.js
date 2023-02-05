c5d2a38a3e436cdb5dee376cb5f9d808736d23ed
0xpranjl <Pranjal.maurya402@gmail.com>
2023-02-05 08:38:25 +0530

    first n final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [address,setaddress]=useState("no connected address");
+  const [btnflag,setbtnflag]=useState(true);
+  useEffect(()=>{
+  isConnected();
+  },[]);
+ async function connect(){
+  if(window.ethereum){
+   const address= await window.ethereum.request({method:'eth_requestAccounts'});
+   console.log(address[0]);
+   setaddress(address[0]);
+  }
+ } 
+ async function isConnected() {
+  const accounts = await window.ethereum.request({method: 'eth_accounts'});       
+  if (accounts.length) {
+  setaddress(accounts[0]);
+  setbtnflag(false);
+  } else {
+    setaddress("no connected address");
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {btnflag?<button onClick={()=>{
+        connect()
+      }}>connect</button>:<></>}
+      <h1>{address}</h1>
     </div>
   );
 }
